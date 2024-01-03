bcc68dbe1827938d2bab405669af2ee5736c4528
sunyoung <skditjsdud35@naver.com>
2024-01-04 03:23:38 +0900

    Feat: 보유 뱃지 페이지 퍼블리싱

---
+++ src/app/mypage/badge/(components)/section/BadgeListSection.tsx
@@ -0,0 +1,55 @@
+import MidiumHead from "@/app/mypage/(components)/head/MidiumHead"
+import BadgeCard from "../card/BadgeCard"
+import SelectBoxButton from "@/app/mypage/(components)/button/SelectBoxButton"
+import PaginationButton from "@/app/mypage/(components)/button/PaginationButton"
+
+const BadgeListSection = () => {
+    return (
+        <div className="pt-[94px]">
+            <MidiumHead title="뱃지 목록" content="스팩폴리오 활동을 통해 뱃지를 모아보세요." />
+            <div className="flex justify-end space-x-4 py-[24px]">
+                <SelectBoxButton options={['전체', '오래된순', '인기순']} />
+            </div>
+            <div className="">
+                <MidiumHead title="첫 걸음" content="" />
+                <div className="grid grid-cols-5 gap-6 pt-[38px] pb-[108px]">
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                </div>
+            </div>
+            <div className="">
+                <MidiumHead title="커뮤니티 활동" content="" />
+                <div className="grid grid-cols-5 gap-6 pt-[38px] pb-[108px]">
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                </div>
+            </div>
+            <div className="">
+                <MidiumHead title="프로젝트 활동" content="" />
+                <div className="grid grid-cols-5 gap-6 pt-[38px]">
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                    <BadgeCard />
+                </div>
+            </div>
+            <div className="pt-[80px]">
+                <PaginationButton />
+            </div>
+        </div>
+    )
+}
+
+export default BadgeListSection
