5b1eafbc3cfbace33c59ff4baf205ccb11dac488
sunyoung <skditjsdud35@naver.com>
2024-01-04 01:34:15 +0900

    Feat: 커뮤니티 관리 페이지 퍼블리싱

---
+++ src/app/mypage/community/(components)/section/MyCommentSection.tsx
@@ -0,0 +1,26 @@
+import BasicButton from "@/app/mypage/(components)/button/BasicButton"
+import PostCard from "../card/PostCard"
+import PaginationButton from "@/app/mypage/(components)/button/PaginationButton"
+import SelectBoxButton from "@/app/mypage/(components)/button/SelectBoxButton"
+
+const MyCommentSection = () => {
+    return (
+        <>
+            <div className="flex justify-end space-x-4 py-[24px]">
+                <SelectBoxButton options={['최신순', '오래된순', '인기순']} />
+            </div>
+            <div className="grid gap-6">
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+                <PostCard />
+            </div>
+            <div className="pt-[80px]">
+                <PaginationButton />
+            </div>
+        </>
+    )
+}
+
+export default MyCommentSection
