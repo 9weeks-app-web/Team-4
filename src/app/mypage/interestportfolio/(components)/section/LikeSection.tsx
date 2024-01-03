d55a1652a12a75dc3330e4382a5ac4344ed489e7
sunyoung <skditjsdud35@naver.com>
2024-01-03 23:49:49 +0900

    Feat: 관심 포트폴리오 페이지 퍼블리싱

---
+++ src/app/mypage/interestportfolio/(components)/section/LikeSection.tsx
@@ -0,0 +1,31 @@
+
+import PortfolioCard from "@/app/mypage/(components)/card/common/PortfolioCard"
+import PaginationButton from "@/app/mypage/(components)/button/PaginationButton"
+import BasicButton from "@/app/mypage/(components)/button/BasicButton"
+
+const LikeSection = () => {
+    return (
+        <>
+            <div className="flex justify-end space-x-4 pt-[24px] pb-[48px]">
+                <BasicButton content="추가" />
+                <BasicButton content="편집" />
+            </div>
+            <div className="grid grid-cols-3 gap-6">
+                <PortfolioCard />
+                <PortfolioCard />
+                <PortfolioCard />
+                <PortfolioCard />
+                <PortfolioCard />
+                <PortfolioCard />
+                <PortfolioCard />
+                <PortfolioCard />
+                <PortfolioCard />
+            </div>
+            <div className="pt-[80px]">
+                <PaginationButton />
+            </div>
+        </>
+    )
+}
+
+export default LikeSection
