540592b6db88348d36b87b3dc5e114ccb86e98a5
thwlckd <thwlckd@naver.com>
2024-01-06 21:55:58 +0900

    Style: 버튼 인터랙션 수정

--- src/app/gathering/(components)/button/ButtonRound.tsx
+++ src/app/gathering/(components)/button/ButtonRound.tsx
@@ -1,11 +1,13 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 
 interface ButtonRound {
   content: string;
   theme: 'white' | 'primary' | 'neutral';
+  link?: string;
 }
 
-const ButtonRound = ({ content, theme }: ButtonRound) => {
+const ButtonRound = ({ content, theme, link }: ButtonRound) => {
   const className = clsx(
     'px-[30px]',
     'py-[15px]',
@@ -13,14 +15,33 @@ const ButtonRound = ({ content, theme }: ButtonRound) => {
     'border',
     'rounded-[100px]',
     'bg-transparent',
-    // 'hover:text-neutral-0',
-    // 'hover:bg-primary-90',
-    theme === 'white' && ['border-neutral-0', 'text-neutral-0'],
-    theme === 'primary' && ['border-primary-80', 'text-primary-80'],
-    theme === 'neutral' && ['bg-neutral-70', 'text-neutral-0'],
+    theme === 'white' && [
+      'border-neutral-0',
+      'text-neutral-0',
+      'hover:bg-primary-0',
+      'hover:text-neutral-90',
+      'active:bg-neutral-5',
+    ],
+    theme === 'primary' && [
+      'border-primary-80',
+      'text-primary-80',
+      'hover:text-neutral-0',
+      'hover:bg-primary-80',
+      'active:bg-primary-90',
+    ],
+    theme === 'neutral' && [
+      'bg-neutral-70',
+      'text-neutral-0',
+      'hover:bg-neutral-80',
+      'active:bg-neutral-90',
+    ],
   );
 
-  return <button className={className}>{content}</button>;
+  return (
+    <button className={className}>
+      {link ? <Link href={link}>{content}</Link> : content}
+    </button>
+  );
 };
 
 export default ButtonRound;
